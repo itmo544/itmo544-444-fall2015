aa192b5004d811b9bd229c8123090643d93c692d
Sultan <deepclean911@yahoo.com>
2015-10-28 00:31:44 -0500

    changed mp1-jrh tp mp1-sb in all .php files

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
-    Send this file: <input name="userfile" type="file" /><br />
-Enter Email of user: <input type="email" name="useremail"><br />
-Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone">
+    Send: <input name="userfile" type="file" /><br />
+Email Address: <input type="email" name="useremail"><br />
+Phone # (1-XXX-XXX-XXXX): <input type="phone" name="phone">
 
 
 <input type="submit" value="Send File" />
