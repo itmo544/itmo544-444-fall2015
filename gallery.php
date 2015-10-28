36bc221f9d3ee5fe13289663c12e0599ef3d2531
Sultan <deepclean911@yahoo.com>
2015-10-28 01:43:25 -0500

    added an epty space

--- gallery.php
+++ gallery.php
@@ -37,6 +37,7 @@ if (mysqli_connect_errno()) {
 //below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
 $link->real_query("SELECT * FROM items WHERE email = '$email'");
 //$link->real_query("SELECT * FROM items");
+
 $res = $link->use_result();
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
