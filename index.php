76930499575d233eb5241f4bd7d43edfcdc291aa
Sultan <deepclean911@yahoo.com>
2015-10-28 00:48:37 -0500

    Changed result.php to submit.php

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php session_start(); ?>
 <html>
-<head><title>Hello app</title>
-</head>
+	<head><title>Hello app</title>
+	</head>
+
 <body>
+<!-- The data encoding type, enctype, MUST be specified as below -->
+	<form enctype="multipart/form-data" action="submit.php" method="POST">
+   
+		<!-- MAX_FILE_SIZE must precede the file input field -->
+		<input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+
+		<!-- Name of input element determines name in $_FILES array -->
+		Send: <input name="userfile" type="file" /><br />
+		Email Address: <input type="email" name="useremail"><br />
+		Phone # (1-XXX-XXX-XXXX): <input type="phone" name="phone">
+
+		<input type="submit" value="Send File" />
+	</form>
+	<hr />
 
 <!-- The data encoding type, enctype, MUST be specified as below -->
-<form enctype="multipart/form-data" action="result.php" method="POST">
-    <!-- MAX_FILE_SIZE must precede the file input field -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
-    <!-- Name of input element determines name in $_FILES array -->
-    Send: <input name="userfile" type="file" /><br />
-Email Address: <input type="email" name="useremail"><br />
-Phone # (1-XXX-XXX-XXXX): <input type="phone" name="phone">
-
-
-<input type="submit" value="Send File" />
-</form>
-<hr />
-<!-- The data encoding type, enctype, MUST be specified as below -->
-<form enctype="multipart/form-data" action="gallery.php" method="POST">
+	<form enctype="multipart/form-data" action="gallery.php" method="POST">
     
-Enter Email of user for gallery to browse: <input type="email" name="email">
-<input type="submit" value="Load Gallery" />
-</form>
-
-
+	Enter Email of user for gallery to browse: <input type="email" name="email">
+	<input type="submit" value="Load Gallery" />
+	</form>
 </body>
 </html>
 
