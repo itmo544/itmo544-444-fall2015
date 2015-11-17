9d5af6c721faf8d17306f4269fd3665daf0c3852
Sultan <deepclean911@yahoo.com>
2015-11-16 20:09:27 -0600

    removed 3306

--- submit.php
+++ submit.php
@@ -64,10 +64,10 @@ $result = $rds->describeDBInstances([
 ]);
 
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
-print "============\n". $endpoint . "================";
+#print "============\n". $endpoint . "================";
 
 //echo "begin database";
-$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords",3306) or die("Error " . mysqli_error($link));
+$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords") or die("Error " . mysqli_error($link));
 
 // check connection
 if (mysqli_connect_errno()) {
