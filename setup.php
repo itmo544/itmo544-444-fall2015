1c24efe922d6e3dd7f1f10589980e8c2128b71ea
Sultan <deepclean911@yahoo.com>
2015-11-21 21:03:04 -0600

    trying to also create table from setup.php just incase if missed by launch.sh

--- setup.php
+++ setup.php
@@ -1,5 +1,4 @@
 <?php
-/*
 
 // Start the session
 require 'vendor/autoload.php';
@@ -10,23 +9,17 @@ $rds = new Aws\Rds\RdsClient
    	'region'  => 'us-east-1',
 ]);
 
-$s3 = new Aws\S3\S3Client
-([
-   	'version' => 'latest',
-   	'region'  => 'us-east-1'
-]);
 
-*/
-
+/*
 //Create Database Instance
-/*$result = $rds->createDBInstance
+$result = $rds->createDBInstance
 ([
     'AllocatedStorage' => 5, //MIN REQUIRED 5GB 
     'DBInstanceClass' => 'db.t1.micro', // REQUIRED
     'DBInstanceIdentifier' => 'mp1-sb', // REQUIRED
     'DBName' => 'customerrecords',
     'Engine' => 'MySQL', // REQUIRED
-    'EngineVersion' => '5.6.23', // Version 5.6 and up is Required for Read Replics
+    'EngineVersion' => '5.6.23', // Version 5.6 and up is Required for Read Replics or leave blank for latest Engine Ver.
     'MasterUserPassword' => 'letmein888',
     'MasterUsername' => 'controller',
     'PubliclyAccessible' => true,
@@ -43,23 +36,23 @@ $rrresult = $rds->createDBInstanceReadReplica
 	'SourceDBInstanceIdentifier' => 'mp1-sb', //DB instance name that will act as source 
 	'PubliclyAccessible' => true, //true specifies an Internet-facing instance with a publicly resolvable DNS name
 ]);	
-*/
 
 
 #Wait untill Database is created
-#$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'mp1-sb',]);
-#print "RDS DB Successfully Created: \n";
-#print_r($rds);
+	$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'mp1-sb',]);
+	print "RDS DB Successfully Created: \n";
+	print_r($rds);
+*/
 
-/*
 // Create a table 
 $result = $rds->describeDBInstances
-	([
-    	'DBInstanceIdentifier' => 'mp1-sb',
-	]);
+([
+	'DBInstanceIdentifier' => 'mp1-sb',
+]);
+
 
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
-print "============\n". $endpoint . "================\n";
+print "\n============\n". $endpoint . "\n================\n";
 
 
 //echo "begin database";
@@ -76,23 +69,25 @@ if (mysqli_connect_errno()) {
 
 echo "succssfully connected to database";
 
-#create table comments
-$sql = "CREATE TABLE items
+#create table items
+$sql = "CREATE TABLE IF NOT EXISTS items
 (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	uname VARCHAR2(20) NOT NULL,
-	email VARCHAR2(30) NOT NULL,
-	phone VARCHAR2(20) NOT NULL,
-	s3rawurl VARCHAR2(255) NOT NULL,
-	s3finishedurl VARCHAR2(255) NOT NULL,
-	filename VARCHAR2(255) NOT NULL,
+	uname VARCHAR(20) NOT NULL,
+	email VARCHAR(30) NOT NULL,
+	phone VARCHAR(20) NOT NULL,
+	s3rawurl VARCHAR(255) NOT NULL,
+	s3finishedurl VARCHAR(255) NOT NULL,
+	filename VARCHAR(255) NOT NULL,
 	status TINYINT(3)CHECK(state IN(0,1,2)),
-	date DATETIME DEFAULT CURRENT_TIMESTAMP
+	cdate DATETIME DEFAULT CURRENT_TIMESTAMP
 )";
 
 $con->query($sql);
 
 echo "table created";
-*/	
+
+#*/	
+
 ?>
 
