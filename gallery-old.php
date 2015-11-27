8b9d84d11bc101088839e9f04755b577f6672997
Sultan <deepclean911@yahoo.com>
2015-11-26 21:48:04 -0600

    trying to add default gallery file

---
+++ gallery-old.php
@@ -0,0 +1,54 @@
+<html>
+<head><title>Gallery</title>
+</head>
+<body>
+
+<?php
+session_start();
+$email = $_POST["email"];
+echo $email;
+require 'vendor/autoload.php';
+
+//TESTING THESE TWO ECHO STATEMENTS
+echo $_POST['useremail'];
+echo $_POST['userfile']['tmp_name'];
+
+#New Rds Code
+use Aws\Rds\RdsClient;
+$rds = new Aws\Rds\RdsClient([
+    'version' => 'latest',
+    'region'  => 'us-east-1',
+]);
+
+$result = $client->describeDBInstances(array(
+    'DBInstanceIdentifier' => 'mp1-sb',
+));
+
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+#print "============". $endpoint . "================";
+
+//echo "begin database";
+$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords",3306) or die("Error " . mysqli_error($link));
+
+/*
+// check connection
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+*/
+
+//below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
+$link->real_query("SELECT * FROM items WHERE email = '$email'");
+#$link->real_query("SELECT * FROM items");
+
+$res = $link->use_result();
+echo "Result set order...\n";
+while ($row = $res->fetch_assoc()) {
+    echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
+echo $row['id'] . "Email: " . $row['useremail'];
+}
+$link->close();
+?>
+</body>
+</html>
