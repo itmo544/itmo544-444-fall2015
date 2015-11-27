86f38b4a63271fc30ed08c0ab451d6a5ef1ee495
Sultan <deepclean911@yahoo.com>
2015-11-27 17:34:49 -0600

    removed unnecessary code

--- setup.php
+++ setup.php
@@ -9,41 +9,6 @@ $rds = new Aws\Rds\RdsClient
    	'region'  => 'us-east-1'
 ]);
 
-
-/*
-//Create Database Instance
-$result = $rds->createDBInstance
-([
-    'AllocatedStorage' => 5, //MIN REQUIRED 5GB 
-    'DBInstanceClass' => 'db.t1.micro', // REQUIRED
-    'DBInstanceIdentifier' => 'mp1-sb', // REQUIRED
-    'DBName' => 'customerrecords',
-    'Engine' => 'MySQL', // REQUIRED
-    'EngineVersion' => '5.6.23', // Version 5.6 and up is Required for Read Replics or leave blank for latest Engine Ver.
-    'MasterUserPassword' => 'letmein888',
-    'MasterUsername' => 'controller',
-    'PubliclyAccessible' => true,
-    #'StorageEncrypted' => true || false,
-    #'TdeCredentialArn' => '<string>',
-    #'TdeCredentialPassword' => '<string>',
-    #'VpcSecurityGroupIds' => 'sg-e30e4b84' #['<string>', ...],
-]);
-
-#Create Read Replica - Golden Copy
-$rrresult = $rds->createDBInstanceReadReplica
-([
-	'DBInstanceIdentifier' => 'mp1-sb-rr', //Unique Name to identify RR DB Instance
-	'SourceDBInstanceIdentifier' => 'mp1-sb', //DB instance name that will act as source 
-	'PubliclyAccessible' => true, //true specifies an Internet-facing instance with a publicly resolvable DNS name
-]);	
-
-
-#Wait untill Database is created
-	$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'mp1-sb',]);
-	print "RDS DB Successfully Created: \n";
-	print_r($rds);
-*/
-
 // Create a table 
 $result = $rds->describeDBInstances
 ([
@@ -56,9 +21,9 @@ $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 
 
 //echo "begin database";
-$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords") or die("Error " . mysqli_error($link)); 
+$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords",3306) or die("Error " . mysqli_error($link)); 
 
-echo "Here is the result: " . $link;
+#echo "Here is the result: " . $link;
 
 /*
 // check connection
@@ -67,8 +32,7 @@ if (mysqli_connect_errno()) {
     exit();
 }
 */
-
-echo "succssfully connected to database and now creating table";
+#echo "succssfully connected to database and now creating table";
 
 #create table items
 $sql = "CREATE TABLE IF NOT EXISTS items
@@ -87,9 +51,7 @@ $sql = "CREATE TABLE IF NOT EXISTS items
 
 $link->query($sql);
 $link->close();
-echo "table created";
-
-#*/	
+#echo "table created";
 
 ?>
 
