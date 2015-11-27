03c09738864badd55bb593a565e8a0dfdf5f8743
Sultan <deepclean911@yahoo.com>
2015-11-26 22:18:49 -0600

    removed 3306

--- setup.php
+++ setup.php
@@ -56,7 +56,7 @@ $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 
 
 //echo "begin database";
-$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords",3306) or die("Error " . mysqli_error($link)); 
+$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords") or die("Error " . mysqli_error($link)); 
 
 #echo "Here is the result: " . $link;
 
