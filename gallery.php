9f3bbafc9332aeb789211d11d48fd06e2044c28a
Sultan <deepclean911@yahoo.com>
2015-11-27 15:08:39 -0600

    default gallery.php

--- gallery.php
+++ gallery.php
@@ -4,13 +4,12 @@
 </head>
 
 <body>
+<h1> Welcome to your Galler</h1>
 
 <?php
 session_start();
-
 $email = $_POST["email"];
 echo $email;
-
 require 'vendor/autoload.php';
 
 use Aws\Rds\RdsClient;
@@ -44,17 +43,13 @@ if (mysqli_connect_errno()) {
 
 //below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
 $link->real_query("SELECT * FROM items WHERE email = '$email'");
-#$link->real_query("SELECT * FROM items");
+//$link->real_query("SELECT * FROM items");
 $res = $link->use_result();
-
 echo "Result set order...\n";
-
-while ($row = $res->fetch_assoc()) 
-{
-	echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
-	echo $row['id'] . "Email: " . $row['email'];
+while ($row = $res->fetch_assoc()) {
+    echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
+echo $row['id'] . "Email: " . $row['email'];
 }
-
 $link->close();
 ?>
 </body>
