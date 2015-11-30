940b1be9d622cc67cddaac5f2f190605cca2ab99
Sultan <deepclean911@yahoo.com>
2015-11-30 00:08:54 -0600

    done with variables

--- introspection.php
+++ introspection.php
@@ -21,6 +21,12 @@ session_start();
 <?php
 require 'vendor/autoload.php';
 
+//variable for db connection
+$dbuser = 'controller';
+$dbpass = 'letmein888';
+$dbname = 'customerrecords';
+
+use Aws\Rds\RdsClient;
 $rdsrr = new Aws\Rds\RdsClient
 ([
    	'version' => 'latest',
@@ -42,11 +48,14 @@ $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 $link = mysqli_connect($endpoint,"controller","letmein888","customerrecords",3306) or die("Error " . mysqli_error($link)); 
 
 
-//s3 - call variables from submit.php
+//s3 - call variables
 
-$uploaddir = '/tmp/';
+$uploaddir = '/tmp/DBbackup';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-$dbbucket = uniqid("php-sb-dbbucket",false);
+$dbbackup = uniqid("php-sb-dbbackup",false);
+$dbpath = $uploaddir.dbbucket. '.' . sql;
+$sqlcon = "mysqldump --user=$dbuser --password$dbpass --host=$endpoint $dbname > $dbpath";
+exec($sqlcon);
 
 echo '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) 
@@ -62,18 +71,26 @@ echo 'Here is some more debugging info:';
 print_r($_FILES);
 print "</pre>";
 
+$s3 = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
 
+# AWS PHP SDK version 3 create bucket
+$dbbucket = uniqid("php-sb-dbbucket",false);
+$result = $s3->createBucket
+    ([
+        'ACL' => 'public-read',
+        'Bucket' => $dbbucket
+    ]);
 
-
-
-
-
-
-
-
-
-
-
+// PHP version 3 for putting object in s3
+$result = $s3->putObject([
+        'ACL' => 'public-read',
+        'Bucket' => $dbbucket,
+        'Key' => $dbpath,
+        'SourceFile' => $dbpath
+]); 
 
 ?>
 
