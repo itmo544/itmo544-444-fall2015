99e96d2c94b13e0f8fca055474b838114ccb2546
Sultan <deepclean911@yahoo.com>
2015-11-29 20:45:43 -0600

    added css

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php session_start(); ?>
-<html>
-	<head><title>Hello app</title>
-	</head>
+
+#The following code borrowed from http://www.w3schools.com/bootstrap/bootstrap_get_started.asp
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ITMO544-444 Final Project</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
 
 <body>
+<div class="container">
+  <h1>Form WD40</h1>
+  <p>All Fields Required</p>
+
 <!-- The data encoding type, enctype, MUST be specified as below -->
 	<form enctype="multipart/form-data" action="submit.php" method="POST">
    
@@ -25,6 +39,7 @@
 	Enter Email of user for gallery to browse: <input type="email" name="email">
 	<input type="submit" value="Load Gallery" />
 	</form>
+</div>
 </body>
 </html>
 
